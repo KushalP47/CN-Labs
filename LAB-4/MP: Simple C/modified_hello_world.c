19aaa5e4caa2a4d52aab1853982d1e185407cdda
Kushal Patel <kushal300603@gmail.com>
2024-02-14 11:03:03 +0530

    Till Lab 6

---
+++ LAB-4/MP: Simple C/modified_hello_world.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    char name[10];
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Hello %s!!\n", name);
+    return 0;
+    
+}
