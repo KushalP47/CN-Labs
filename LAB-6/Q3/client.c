1150fed2fe57c921b2a80905f0bfc29163488244
Kushal Patel <kushal300603@gmail.com>
2024-02-15 22:39:36 +0530

    Q3 done

--- LAB-6/Q3/client.c
+++ LAB-6/Q3/client.c
@@ -6,114 +6,117 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
-
+// Now you can use struct hostent
+struct hostent *he;
 #define SERVER_PORT 5432
 #define MAX_LINE 256
 
-void recieveFile(int s, FILE* fptr, int fileSize){
+void receiveFile(int s, FILE* fptr, int fileSize) {
     long int packetSize = 256, totalPackets = (fileSize/256);
-    if(fileSize%256 != 0)
-      totalPackets += 1;
-    
+    if(fileSize % 256 != 0)
+        totalPackets += 1;
+
     int currentPacket = 0;
     char buf[256];
-    printf("Packets Received: %d   Packets Remaining: %d \n", currentPacket, totalPackets);
+    printf("Packets Received: %d   Packets Remaining: %ld \n", currentPacket, totalPackets);
     while(totalPackets >= currentPacket){
-      printf("Packets Received: %d   Packets Remaining: %d \r", currentPacket, totalPackets);
-      int l = recv(s, buf, sizeof(buf), 0);
-      fwrite(buf, 1, l, fptr);
-      currentPacket ++;
+        printf("Packets Received: %d   Packets Remaining: %ld \r", currentPacket, totalPackets);
+        int l = recv(s, buf, sizeof(buf), 0);
+        fwrite(buf, 1, l, fptr);
+        currentPacket++;
     }
     fclose(fptr);
     return;
 }
 
 int main(int argc, char * argv[]){
-  FILE *fp;
-  struct hostent *hp;
-  struct sockaddr_in sin;
-  char *host, *fileName;
-  char buf[MAX_LINE], c;
-  int s;
-  int len;
-  if (argc > 1) {
-    // host = argv[1];
-    // fileName = argv[3];
-    while((c = getopt(argc, argv, "h:f:")) != -1){
-        switch(c){
-        case 'f':
-            fileName = optarg;
-            printf("Client's file name: %s\n", fileName);
-            break;
-        case 'h':
-            host = optarg;
-            printf("Client connected to server: %s\n", host);
-            break;
-        case '?':
-            fprintf(stderr, "usage: %s -p port host\n", argv[0]);
-            exit(1);
+    FILE *fp;
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    char *host, *fileName;
+    char buf[MAX_LINE], c;
+    int s;
+    socklen_t len; // Change to socklen_t
+    if (argc > 1) {
+        while((c = getopt(argc, argv, "h:f:")) != -1){
+            switch(c){
+                case 'f':
+                    fileName = optarg;
+                    printf("Client's file name: %s\n", fileName);
+                    break;
+                case 'h':
+                    host = optarg;
+                    printf("Client connected to server: %s\n", host);
+                    break;
+                case '?':
+                    fprintf(stderr, "usage: %s -p port host\n", argv[0]);
+                    exit(1);
+            }
         }
     }
-  }
-  else {
-    fprintf(stderr, "usage: %s host\n", argv[0]);
-    exit(1);
-  }
-
-  
-  /* translate host name into peer's IP address */
-  hp = gethostbyname(host);
-  if (!hp) {
-    fprintf(stderr, "%s: unknown host: %s\n", argv[0], host);
-    exit(1);
-  }
-  else
-    printf("Client's remote host: %s\n", argv[1]);
-  
-  /* build address data structure */
-  bzero((char *)&sin, sizeof(sin));
-  sin.sin_family = AF_INET;
-  bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
-  sin.sin_port = htons(SERVER_PORT);
-  
-  /* active open */
-  if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("simplex-talk: socket");
-    exit(1);
-  }
-  else
-    printf("Client created socket.\n");
-
-  if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
-    {
-      perror("simplex-talk: connect");
-      close(s);
-      exit(1);
+    else {
+        fprintf(stderr, "usage: %s host\n", argv[0]);
+        exit(1);
     }
-  else
-    printf("Client connected.\n");
-  
-  send(s, fileName, 256, 0);
-  printf("Client sent: %s file request\n", fileName);
-  recv(s, buf, sizeof(buf), 0);
-  printf("file size %s", buf);
-  int fileSize = atoi(buf);
-  printf("File size to recieve: %d \n", fileSize);
-  if(fileSize == -1){
-    printf("File doesn't exists\n");
+
+    /* translate host name into peer's IP address */
+    hp = gethostbyname(host);
+    if (!hp) {
+        fprintf(stderr, "%s: unknown host: %s\n", argv[0], host);
+        exit(1);
+    }
+    else
+        printf("Client's remote host: %s\n", argv[1]);
+
+    /* build address data structure */
+    bzero((char *)&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+    sin.sin_port = htons(SERVER_PORT);
+
+    /* active open */
+    if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("simplex-talk: socket");
+        exit(1);
+    }
+    else
+        printf("Client created socket.\n");
+
+    sendto(s, fileName, strlen(fileName), 0, (struct sockaddr *)&sin, sizeof(sin));
+    printf("Client sent: %s file request\n", fileName);
+
+    len = sizeof(sin); // Correct assignment
+    if ((recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&sin, &len)) < 0) {
+        perror("simplex-talk: recvfrom");
+        exit(1);
+    }
+
+    printf("File size: %s\n", buf);
+    int fileSize = atoi(buf);
+    printf("File size to receive: %d bytes\n", fileSize);
+
+    if(fileSize == -1){
+        printf("File doesn't exist.\n");
+        close(s);
+        return 0;
+    }
+
+    char path[MAX_LINE] = "./responses/";
+    strcat(path, fileName);
+    printf("Saving file to: %s\n", path);
+    FILE* fptr = fopen(path, "wb");
+    if (!fptr) {
+        perror("Failed to open file");
+        close(s);
+        exit(1);
+    }
+
+    receiveFile(s, fptr, fileSize);
+    printf("File Received.\n");
     close(s);
+
     return 0;
-  }
-
-  char path[MAX_LINE] = "./responses/";
-  strcat(path, fileName);
-  printf("%s", path);
-  FILE* fptr = fopen(path, "wb");
-  printf("File size to recieve: %d \n", fileSize);
-  recieveFile(s, fptr, fileSize);
-  printf("File Received\n");
-  close(s);
-
 }
--- LAB-6/Q3/server.c
+++ LAB-6/Q3/server.c
@@ -6,11 +6,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
 
 #define SERVER_PORT 5432
-#define MAX_PENDING 5
 #define MAX_LINE 256
 
 int findSize(char *file_name) 
@@ -19,7 +17,7 @@ int findSize(char *file_name)
     // opening the file in read mode 
     FILE* fp = fopen(file_name, "r"); 
   
-    // checking if the file exist or not 
+    // checking if the file exists or not 
     if (fp == NULL) { 
         // printf("File Not Found!\n"); 
         return -1; 
@@ -36,83 +34,76 @@ int findSize(char *file_name)
     return res; 
 } 
 
-void sendFile(int s, char *fileName, long int fileSize){
-	int packetSize = 256, totalPackets = (fileSize/256);
-	if(fileSize%256 != 0)
-		totalPackets++;
-	
-	int currentPacket = 0;
-	char buf[256];
-	FILE* fptr = fopen(fileName, "rb");
-	printf("%d %d\n", currentPacket, totalPackets);
-	while(totalPackets >= currentPacket){
-		printf("Packets Send: %d Packets Remaining: %ld \r", currentPacket, totalPackets);
-		fflush(stdout);
-		int re = fread(buf, 1, sizeof(buf), fptr);
-		send(s, buf, re, 0);
-		sleep(0.1);
-		currentPacket++;
-	}
-	printf("\nFile Sent\n");
-	fclose(fptr);
-	return;
+void sendFile(int s, struct sockaddr *client_addr, socklen_t client_len, char *fileName, long int fileSize) {
+    int packetSize = 256, totalPackets = (fileSize/256);
+    if(fileSize % 256 != 0)
+        totalPackets++;
+
+    int currentPacket = 0;
+    char buf[256];
+    FILE* fptr = fopen(fileName, "rb");
+    printf("%d %d\n", currentPacket, totalPackets);
+    while(totalPackets >= currentPacket){
+        printf("Packets Send: %d Packets Remaining: %ld \r", currentPacket, totalPackets);
+        fflush(stdout);
+        int re = fread(buf, 1, sizeof(buf), fptr);
+        sendto(s, buf, re, 0, client_addr, client_len);
+        sleep(0.1);
+        currentPacket++;
+    }
+    printf("\nFile Sent\n");
+    fclose(fptr);
 }
 
 int main(){
 
-  struct sockaddr_in sin;
-  char buf[256], *fileName;
-  socklen_t len;
-  int s, new_s;
-  char str[INET_ADDRSTRLEN];
+    struct sockaddr_in sin, client_addr;
+    char buf[MAX_LINE], *fileName;
+    socklen_t len = sizeof(client_addr);
+    int s;
+    char str[INET_ADDRSTRLEN];
 
-  /* build address data structure */
-  bzero((char *)&sin, sizeof(sin));
-  sin.sin_family = AF_INET;
-  sin.sin_addr.s_addr = INADDR_ANY;
-  sin.sin_port = htons(SERVER_PORT);
-  
-  /* setup passive open */
-  if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("simplex-talk: socket");
-    exit(1);
-  }
- 
-  inet_ntop(AF_INET, &(sin.sin_addr), str, INET_ADDRSTRLEN);
-  printf("Server is using address %s and port %d.\n", str, SERVER_PORT);
+    /* build address data structure */
+    bzero((char *)&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(SERVER_PORT);
 
-  if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
-    perror("simplex-talk: bind");
-    exit(1);
-  }
-  else
-    printf("Server bind done.\n");
-
-  listen(s, MAX_PENDING);
-  
-  while(1){
-    if ((new_s = accept(s, (struct sockaddr *)&sin, &len)) < 0) {
-      perror("simplex-talk: accept");
-      exit(1);
+    /* setup passive open */
+    if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("simplex-talk: socket");
+        exit(1);
     }
-    printf("Server Listening.\n");
 
-    while(len = recv(new_s, buf, 256, 0)){
-      fileName = buf;
-        printf("Server received: %s file request \n", buf);
+    inet_ntop(AF_INET, &(sin.sin_addr), str, INET_ADDRSTRLEN);
+    printf("Server is using address %s and port %d.\n", str, SERVER_PORT);
+
+    if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+        perror("simplex-talk: bind");
+        exit(1);
+    }
+    else
+        printf("Server bind done.\n");
+
+    while(1){
+        printf("Server Listening.\n");
+
+        if ((recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&client_addr, &len)) < 0) {
+            perror("simplex-talk: recvfrom");
+            exit(1);
+        }
+
+        fileName = buf;
+        printf("Server received: %s file request \n", fileName);
         int fileSize = findSize(fileName);
-				char str[3];
-				sprintf(str, "%d", fileSize);
-			  printf("%d %s\n", fileSize, str);
-				send(new_s, str, strlen(str), 0);
-        if(fileSize == -1){
-						close(new_s);
-						break;
-				}
-        sendFile(new_s, fileName, fileSize);
-        close(new_s);
-        break;
+        char str[3];
+        sprintf(str, "%d", fileSize);
+        printf("%d %s\n", fileSize, str);
+        sendto(s, str, strlen(str), 0, (struct sockaddr *)&client_addr, len);
+        if(fileSize == -1)
+            continue;
+        sendFile(s, (struct sockaddr *)&client_addr, len, fileName, fileSize);
     }
-  }
-  
+
+    close(s);
 }
